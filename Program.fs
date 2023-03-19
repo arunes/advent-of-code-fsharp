645219ca9516fd2f12220fdee24ea8db9ad58cda
arunes <ali@arunes.com>
2023-03-19 00:47:17 -0500

    feat: calling results from program.fs

--- Program.fs
+++ Program.fs
@@ -1,2 +1,15 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module Day1 = Puzzles.Year2015.Day01
+module Day2 = Puzzles.Year2015.Day02
+
+// 2015
+// Day 1
+let year2015Day1Part1 = Day1.findFloor
+let year2015Day1Part2 = Day1.findWhenInBasement
+printfn "2015 Day 1, Part 1 result: %d, part 2 result: %d" year2015Day1Part1 year2015Day1Part2
+
+// Day 2
+let year2015Day2Part1 = Day2.allWrapperNeeded
+let year2015Day2Part2 = Day2.allRibbonNeeded
+printfn "2015 Day 2, Part 1 result: %d, part 2 result: %d" year2015Day2Part1 year2015Day2Part2
+
+
